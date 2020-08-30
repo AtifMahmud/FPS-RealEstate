528e18a22cc0b66dec5d5127bc38afaef93fb58f
Atif Mahmud <atifmahmud95@gmail.com>
2020-08-30 14:59:31 -0700

    Object highlight works

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -8,23 +8,43 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform playerBody;
     float yRotation = 0f;
+    bool mouseMovementAllowed = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mouseMovementAllowed = !mouseMovementAllowed;
+            if (Cursor.visible == false)
+            {
+                Cursor.visible = true;
+            }
+            else {
+                Cursor.visible = false;
+            }
+           
+
+        }
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         yRotation -= mouseY;
         yRotation = Mathf.Clamp(yRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if (mouseMovementAllowed)
+        {
+            transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+
     }
 }
--- Assets/Raycast.cs
+++ Assets/Raycast.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+
+    Renderer render;
+    GameObject last; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +22,25 @@ public class Raycast : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit" + hit.transform.gameObject.name);
+
+            if (render != null)
+            {
+                render.material.shader = Shader.Find("Diffuse");
+            }
+
+            last = hit.transform.gameObject;
+            render = last.GetComponent<Renderer>();
+            render.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+            //  Debug.Log("Did Hit" + hit.transform.gameObject.name);
+
+
+           
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
+            // Debug.Log("Did not Hit");
+            
         }
     }
 }
